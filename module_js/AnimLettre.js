a80432953fdc7ecff4391da8f2f1801600b27f65
SimonL <lonelywolf4254@gmail.com>
2019-02-08 17:37:42 -0500

    Console log fonctionne

--- module_js/AnimLettre.js
+++ module_js/AnimLettre.js
@@ -2,92 +2,31 @@ import {
     Util
 } from './Util.js'
 
-
-export class Introduction {
+//Le nom du fichier et le nom de la classe doivent être identiques
+export class AnimLettre {
 
     /**
      * Classe permettant de créer et d'animer une introduction
-     * @param {object} o - contient l'ensemble des mots d'intro
+     * @param {string} lesLettres - contient l'ensemble des mots d'intro
      * @param {DOMElement} elementParent - Conteneur de l'animation
      * @param {function} fonction - l'adresse de la fonction à exécuter après l'animation
          
      }}
      */
 
-    constructor(o, elementParent, fonction) {
-        //Récupérer les valeurs passées en paramètre			
-        this.titrePrincipal = o.titrePrincipal;
-        this.titreSecondaire = o.titreSecondaire
-        this.titreTernaire = o.titreTernaire
-        this.description = o.description
+    constructor(lesLettres, elementParent, fonction) {
+        //Récupérer les valeurs passées en paramètre
+        this.lesLettres = lesLettres			
         this.elmParent = elementParent
-        this.integrerIntro()
+        this.animationLettre(this.lesLettres)
         this.fonction = fonction
     }
 
 
-    integrerIntro() {
-        /* Création des élément DOM qui seront animés. 
-        Les éléments seront intégré dans le conteneur elmParent
-        */
-        console.log('introduction')
-        let elmConteneur = this.creerElement(this.elmParent,
-            'section',
-            '',
-            'introduction')
-
-        let elmPrincipale = this.creerElement(elmConteneur,
-            'div',
-            this.titrePrincipal,
-            'rectangle')
-
-        let elmSecondaire = this.creerElement(elmConteneur,
-            'div',
-            this.titreSecondaire,
-            'rectangle')
-
-
-        let elmTernaire = this.creerElement(elmConteneur,
-            'div',
-            this.titreTernaire,
-            'rectangle')
-
-        let elmDescription = this.creerElement(elmConteneur,
-            'div',
-            this.description,
-            'rectangle')
-
-        let elmBouton = this.creerElement(elmConteneur,
-            'button',
-            'Commencer',
-            'bouton')
-        /* On garde une référence sur la fonction terminerIntro */
-        let refTerminerIntro = this.terminerIntro.bind(this)
-        elmBouton.addEventListener('mousedown', this.terminerIntro.bind(this))
+    animationLettre(lesLettres) {
+       console.log(`lesLettres =  ${lesLettres}`);
     }
 
-    creerElement(elmParent, balise, contenu, classCSS) {
-        console.log(balise)
-        let noeud = document.createElement(balise)
-        if (contenu != '') {
-            noeud.innerHTML = contenu
-        }
-        if(classCSS != ''){
-            noeud.classList.add(classCSS)
-        }
-        noeud.classList.add(classCSS)
-        elmParent.appendChild(noeud)
-        return noeud
-    }
-
-    terminerIntro(evt) {
-        this.elmParent.firstChild.classList.add('deplacementContenuIntro')
-        this.elmParent.firstChild.addEventListener('animationend', this.passerVersAnimationSuivante.bind(this))
-    }
-
-    passerVersAnimationSuivante(evt) {
-        Util.detruireTousLesNoeud(this.elmParent, this.elmParent)
-        this.fonction()
-    }
+    
 
 }
